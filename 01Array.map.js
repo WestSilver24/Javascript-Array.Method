714fd726e7e64af5ba57ab6841fe24f7964dd3f9
jowestsilver <jowestsilver@smartscore.kr>
2022-04-18 13:33:40 +0900

    220418

--- 01Array.map.js
+++ 01Array.map.js
@@ -1,10 +1,43 @@
 // Array.prototype.map()
-// ㄴ map() 메소드는 배열 내의 모든 요소 각각에 대하여 주어진 함수를 호출한 결과를 모아 새로운 배열을 반환
-// ㄴ 배열의 각 원소별로 지정된 함수를 실행한 결과로 구성된 새로운 배열을 반환
-//
+// ㄴ map() 메소드는 배열의 모든 요소 각각에 주어진 함수를 호출한 결과로 구성된 새로운 배열을 반환한다.
 // MDN => https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/map
 
+// 새로운 배열을 반환: [2, 5, 10, 23]
 const arr = [ 1, 4, 9, 22 ];
-const newArray = arr.map( x => x * 2);
-console.log(newArray);
-// [2, 8, 18, 44]
+const newArr = arr.map( x => x + 1);
+console.log(newArr);
+
+
+
+let elements = [
+  'West',
+  'Silver',
+  'Cho',
+  'Developer'
+];
+
+// 1. 일반함수: [8, 12, 6, 18]
+elements.map(function(element) {
+    return element.length * 2;
+});
+
+// 2. 화살표함수
+elements.map((element) => {
+    return element.length * 2;
+});
+
+// 파라미터가 하나만 있을 때는 주변괄호 '()' 생략 가능
+elements.map(element => {
+    return element.length * 2;
+});
+
+// 화살표 함수의 유일한 문장이 'return'일때 'return'과 
+// 중괄호 '{}' 생략 가능
+elements.map(element => element.length * 2);
+
+// 3. destructing 매개변수 사용 : 속성이 하나 일때
+// length = 속성, lengthFooArX = 변수명
+elements.map(({ length: lengthFooArX }) => lengthFooArX * 2);
+// destructing 파라미터 할당
+// 리터럴 이름은 꺼내오고 싶은 속성이름 자체로 사용
+elements.map(({ length }) => length * 2);
---
+++ 02Array.forEach.js
@@ -0,0 +1,20 @@
+// Array.prototype.forEach()
+// ㄴ forEach() 메소드는 주어진 함수를 배열 요소 각각에 대해 실행한다.
+// MDN => https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+
+
+// 배열의 각 요소에 대해 오름차순으로 한 번 씩 실행: 2, 4, 6
+const arr = [1, 2, 3];
+arr.forEach(value => console.log(value * 2));
+
+
+
+// for 반복문을 forEach()로 바꾸기
+const items = ['item1', 'item2', 'item3'];
+const copy = [];
+
+for (let i = 0; i < items.length; i++){
+    copy.push(items[i]);
+}
+
+items.forEach(item => copy.push(item));
